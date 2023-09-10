a0c51a1b25e7e23423c38378056298b7bdeff93f
marpaWilliamIII <113428435+marpaWilliamIII@users.noreply.github.com>
2023-09-10 09:14:35 +0800

    added first test code

---
+++ ARRAY-C#/ARRAY-C#/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARRAY_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("test successfull");
+            Console.ReadKey();
+        }
+    }
+}
