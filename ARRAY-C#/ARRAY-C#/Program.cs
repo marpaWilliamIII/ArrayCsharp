636a1f90fe2b98e5683e17de75409f32e7abdc7f
marpaWilliamIII <113428435+marpaWilliamIII@users.noreply.github.com>
2023-09-10 11:45:06 +0800

    "added the matrix problem"

--- ARRAY-C#/ARRAY-C#/Program.cs
+++ ARRAY-C#/ARRAY-C#/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace ARRAY_C_
 {
@@ -11,6 +14,32 @@ namespace ARRAY_C_
         static void Main(string[] args)
         {
             Console.Write("test successfull");
+
+        //multidimensional array
+//        Exercise: Matrix Transposition
+
+//You are given a square matrix(an N x N 2D array) representing an image.Rotate the image by 90 degrees(clockwise).
+
+//Write a function called rotate_matrix that takes an N x N matrix as input and modifies it in-place such that it is rotated 90 degrees clockwise.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             Console.ReadKey();
         }
     }
