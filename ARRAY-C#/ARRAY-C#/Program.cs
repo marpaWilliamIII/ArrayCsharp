7e5c612d3fee1ac0b9d5da5ec45bd59ffd857cf6
marpaWilliamIII <113428435+marpaWilliamIII@users.noreply.github.com>
2023-09-10 12:38:12 +0800

    don't know what to do hwhawhahwh

--- ARRAY-C#/ARRAY-C#/Program.cs
+++ ARRAY-C#/ARRAY-C#/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,17 +14,46 @@ namespace ARRAY_C_
     {
         static void Main(string[] args)
         {
-            Console.Write("test successfull");
+            
 
-        //multidimensional array
-//        Exercise: Matrix Transposition
+            //multidimensional array
+            //        Exercise: Matrix Transposition
 
-//You are given a square matrix(an N x N 2D array) representing an image.Rotate the image by 90 degrees(clockwise).
+            //You are given a square matrix(an N x N 2D array) representing an image.Rotate the image by 90 degrees(clockwise).
 
-//Write a function called rotate_matrix that takes an N x N matrix as input and modifies it in-place such that it is rotated 90 degrees clockwise.
+            //Write a function called rotate_matrix that takes an N x N matrix as input and modifies it in-place such that it is rotated 90 degrees clockwise.
+            int row = 3; int col = 3;   
+
+            int[,] content = new int[row, col];
+
+
+            for(int i = 0; i < row; i++)
+            {
+
+
+                for(int j = 0; j < col; j++)
+
+                { Console.Write("enter value for index ["+i+","+j+"]: ");
+                    content[i, j] = int.Parse(Console.ReadLine());
+                   
+                    Console.Write("\r\t");
+                }
 
 
 
+
+
+            }
+         
+
+            void Matrix()
+            {
+
+                
+
+
+
+            }
 
 
 
